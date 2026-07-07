70806bca77506ecc234f8e9082b33dfbfe0fb1a1
Pál Kecskeméti <kecskemetip@gmail.com>
2026-07-07 20:23:46 +0200

    refactor: Simplify "Longest Substring Without Repeating Characters" solution and remove IDE-specific configuration files

--- src/main/java/longest_substring_without_repeating_characters_3/Solution.java
+++ src/main/java/longest_substring_without_repeating_characters_3/Solution.java
@@ -1,21 +1,21 @@
 package longest_substring_without_repeating_characters_3;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int longest = 0;
-        int l = 0;
-        Map<Character, Integer> counter = new HashMap<>();
-        for (int r = 0; r < s.length(); r++) {
-            counter.put(s.charAt(r), counter.getOrDefault(s.charAt(r), 0) + 1);
-            while (counter.get(s.charAt(r)) > 1) {
-                counter.put(s.charAt(l), counter.get(s.charAt(l)) - 1);
-                l += 1;
-            }
-            longest = Math.max(longest, r - l + 1);
-        }
-        return longest;
-    }
+	public int lengthOfLongestSubstring( String s ) {
+		Set<Character> charSet = new HashSet<>();
+		int left = 0;
+		int result = 0;
+		for ( int right = 0; right < s.length(); right++ ) {
+			while ( charSet.contains( s.charAt( right ) ) ) {
+				charSet.remove( s.charAt( left ) );
+				left++;
+			}
+			charSet.add( s.charAt( right ) );
+			result = Math.max( result, right - left + 1 );
+		}
+		return result;
+	}
 }
