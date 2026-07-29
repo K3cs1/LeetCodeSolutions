e2a497041a49f472c8a9e9e23f9e1a6a17f530c7
Pál Kecskeméti <kecskemetip@gmail.com>
2026-07-29 07:34:33 +0200

    refactor: Simplify loop in "Group Anagrams" solution by using enhanced for-loop

--- src/main/java/group_anagrams_49/Solution.java
+++ src/main/java/group_anagrams_49/Solution.java
@@ -5,8 +5,7 @@ import java.util.*;
 public class Solution {
 	public List<List<String>> groupAnagrams( String[] strs ) {
 		Map<String, List<String>> result = new HashMap<>();
-		for ( int i = 0; i < strs.length; i++ ) {
-			String str = strs[ i ];
+		for ( String str : strs ) {
 			char[] charArray = str.toCharArray();
 			Arrays.sort( charArray );
 			String key = Arrays.toString( charArray );
