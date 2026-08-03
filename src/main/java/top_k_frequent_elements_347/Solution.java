7688bdb3e836e7b9a4a0fce4c6e08c7724b09a7e
Pál Kecskeméti <kecskemetip@gmail.com>
2026-08-03 06:34:35 +0200

    Top k frequency elements: Remove main method

--- src/main/java/top_k_frequent_elements_347/Solution.java
+++ src/main/java/top_k_frequent_elements_347/Solution.java
@@ -1,36 +1,50 @@
 package top_k_frequent_elements_347;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Iterator;
 import java.util.Map;
 
 public class Solution {
 
-	public int[] topKFrequent(int[] nums, int k) {
-		Map<Integer, Integer> count = new HashMap<>();
-		List<List<Integer>> freq = new ArrayList<>();
-		for (int i = 0; i < nums.length + 1; i++) {
-			freq.add(new ArrayList<>());
+	public int[] topKFrequent( int[] nums, int k ) {
+		int[] result = new int[ k ];
+		Map<Integer, Integer> numsMap = new HashMap<>();
+		for ( int num : nums ) {
+			numsMap.put( num, numsMap.getOrDefault( num, 0 ) + 1 );
 		}
-		for (int num : nums) {
-			count.put(num, count.getOrDefault(num, 0) + 1);
+		if ( k > numsMap.size() ) {
+			return new int[]{};
 		}
-		for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
-			freq.get(entry.getValue()).add(entry.getKey());
+
+		int maxValue = 0;
+		for ( Map.Entry<Integer, Integer> entry : numsMap.entrySet() ) {
+			maxValue = Math.max( entry.getValue(), maxValue );
 		}
-		List<Integer> res = new ArrayList<>();
-		for (int i = freq.size() - 1; i > 0; i--) {
-			for (int j = 0; j < freq.get(i).size(); j++) {
-				if (!freq.get(i).isEmpty()) {
-					res.add(freq.get(i).get(j));
-					if (res.size() == k) {
-						return res.stream().mapToInt(Integer::valueOf).toArray();
-					}
+		for ( Map.Entry<Integer, Integer> entry : numsMap.entrySet() ) {
+			if ( entry.getValue() == maxValue ) {
+				result[ 0 ] = entry.getKey();
+			}
+		}
+		int counter = 1;
+		while ( counter < k ) {
+			Iterator<Integer> iterator = numsMap.keySet().iterator();
+			while ( iterator.hasNext() ) {
+				if ( iterator.next() == result[ counter - 1 ] ) {
+					iterator.remove();
+				}
+			}
+			maxValue = 0;
+			for ( Map.Entry<Integer, Integer> entry : numsMap.entrySet() ) {
+				maxValue = Math.max( entry.getValue(), maxValue );
+			}
+			for ( Map.Entry<Integer, Integer> entry : numsMap.entrySet() ) {
+				if ( entry.getValue() == maxValue ) {
+					result[ counter++ ] = entry.getKey();
 				}
 			}
 		}
-		return new int[] {};
+
+		return result;
 	}
 
 }
