885ce6a8f7f58ca8bae201ee0cce653f97f61c8a
Pal Kecskemeti <kecskemetip@gmail.com>
2026-08-19 15:35:17 +0200

    Longest Consecutive Sequence: Fix syntax errors, avoid Set
    Replace the Set-based approach (and the broken in-progress edit) with
    a sort-then-scan solution that tracks streak length without using a
    Set.

--- src/main/java/longest_consecutive_sequence_128/Solution.java
+++ src/main/java/longest_consecutive_sequence_128/Solution.java
@@ -1,22 +1,26 @@
 package longest_consecutive_sequence_128;
 
 import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class Solution {
-	public int longestConsecutive( int[] nums ) {
-		Set<Integer> numSet = Arrays.stream( nums ).boxed().collect( Collectors.toSet() );
-		int longest = 0;
+	public int longestConsecutive(int[] nums) {
+		if (nums.length == 0) {
+			return 0;
+		}
 
-		for ( int num : nums ) {
-			if ( !numSet.contains( num - 1 ) ) {
-				int length = 0;
-				while ( numSet.contains( num + length ) ) {
-					length++;
-				}
-				longest = Math.max( length, longest );
+		Arrays.sort(nums);
+		int longest = 1;
+		int current = 1;
+
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] == nums[i - 1]) {
+				continue;
+			} else if (nums[i] == nums[i - 1] + 1) {
+				current++;
+			} else {
+				current = 1;
 			}
+			longest = Math.max(longest, current);
 		}
 		return longest;
 	}
